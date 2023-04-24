bae542af5521cee67924deb8eefed9b504128959
Elsa <elsaryynanen@outlook.com>
2023-04-24 18:52:45 +0300

    adding user repository class

---
+++ twitter_application/src/repositories/user_repository.py
@@ -0,0 +1,20 @@
+
+from db_connection import get_database_connection
+from entities.user import User
+
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def find_all(self):
+        cursor = self._connection.cursor()
+
+        cursor.execute("select * from users")
+
+        rows = cursor.fetchall()
+
+        return [User(row["username"], row["password"]) for row in rows]
+
+
+user_repository = UserRepository(get_database_connection())
+users = user_repository.find_all()
