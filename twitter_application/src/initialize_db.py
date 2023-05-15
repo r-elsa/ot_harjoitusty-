e83601d17640a971443932c0316fd726b6b9fe9c
Elsa <elsaryynanen@outlook.com>
2023-05-15 18:44:31 +0300

    adding tables to db

--- twitter_application/src/initialize_db.py
+++ twitter_application/src/initialize_db.py
@@ -5,8 +5,24 @@ def drop_tables(connection):
     cursor = connection.cursor()
 
     cursor.execute('''
-        drop table if exists users;
+            drop table if exists user;
     ''')
+
+    cursor.execute('''
+            drop table if exists tweet;
+    ''')
+
+    cursor.execute('''
+            drop table if exists likes;
+    ''')
+
+    cursor.execute('''
+            drop table if exists comment;
+        ''')
+
+    cursor.execute('''
+            drop table if exists follow;
+        ''')
 
     connection.commit()
 
@@ -15,10 +31,57 @@ def create_tables(connection):
     cursor = connection.cursor()
 
     cursor.execute('''
-        create table users (
-            username text primary key,
-            password text
+        create table user (
+            id int primary key,
+            name text,
+            username text,
+            password text,           
+            profile_picture text,
+            admin boolean
+        );        
+    ''')
+
+    
+    cursor.execute('''    
+        create table tweet (
+            id int primary key,
+            user_id int,
+            send_time time,
+            message text,
+            picture_url text
+        );  
+    ''')
+
+    
+    cursor.execute(''' 
+        create table like (
+            id int primary key,
+            user_id int,
+            tweet_id int,
+            send_time time
+        );      
+    ''')
+
+    
+    cursor.execute('''
+        create table comment (
+            id int primary key,
+            user_id int,
+            tweet_id int,
+            send_time time,
+            message text    
+        );       
+    ''')
+
+    
+    cursor.execute('''
+        create table follow (
+        id int primary key,
+        follower int,
+        following int,
+        send_time time
         );
+       
     ''')
 
     connection.commit()
